8a5f0ce338e1c97a717cbe0c850aceabb556f187
ng110 <ngriffin1100@gmail.com>
2017-01-02 21:25:44 +0000

    tests, license

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,39 +1,47 @@
+import os
 import pytest
 import requests
+from hypothesis import given, example
+import hypothesis.strategies as st
 
 from TiddlPy import loadtiddlers, searchtiddlers, findtiddlers, wikiedit
 
 
-@pytest.fixture(scope=session)
+@pytest.fixture(scope='session')
 def tiddlywikidotcom():
     filename = 'tempfile.html'
     r = requests.get('http://tiddlywiki.com')
     file = open(filename, 'wb')
     file.write(r.content)
     yield(filename)
-    # remove file
+#    os.remove(filename)
 
-@pytest.fixture(scope=class)
-def dummywiki():
+
+# @given(txt=st.text())
+# def test_reading1(tiddlywikidotcom, txt):
+#     t=loadtiddlers(tiddlywikidotcom, ['HelloThere'])
+#     print(t[0]['title'])
+#     assert t[0]['title'] == 'HelloThere'
+
+def test_reading1(tiddlywikidotcom):
+    t=loadtiddlers(tiddlywikidotcom, ['HelloThere'])
+    print(t[0]['title'])
+    assert t[0]['title'] == 'HelloThere'
     pass
 
-class TestReading():
-    def test_reading1(tiddlywikidotcom):
-        pass
-    def test_reading2(tiddlywikidotcom):
-        pass
 
-class TestWriting():
-    def test_writing1(tiddlywikidotcom):
-        pass
-    def test_writing2(tiddlywikidotcom):
-        pass
+# def test_reading2(tiddlywikidotcom):
+#     pass
 
-class TestIntegrity():
-    def test_integrity1(tiddlywikidotcom):
-        pass
-    def test_integrity2(tiddlywikidotcom):
-        pass
+# def test_writing1(tiddlywikidotcom):
+#     pass
+# def test_writing2(tiddlywikidotcom):
+#     pass
+
+# def test_integrity1(tiddlywikidotcom):
+#     pass
+# def test_integrity2(tiddlywikidotcom):
+#     pass
 
 
     
